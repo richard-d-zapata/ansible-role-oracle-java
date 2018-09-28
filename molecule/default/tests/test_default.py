570f0699e0c5b923798b17b8f6816a083364cc41
Justin Sako <kaos2oak.dev@gmail.com>
2018-09-28 17:34:57 -0600

    Added support for RedHat/CentOS

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,13 +12,21 @@ def test_java_version_file(host):
 
 
 def test_java_installer_filename_file(host):
+    if host.system_info.distribution == "ubuntu":
+        ext = "tar.gz"
+    elif host.system_info.distribution == "centos":
+        ext = "rpm"
     f = host.file('/tmp/java_installer_filename')
     assert f.exists
-    assert f.contains("jdk-11_linux-x64_bin.tar.gz\n")
+    assert f.contains("jdk-11_linux-x64_bin." + ext + "\n")
 
 
 def test_installer_exists(host):
-    f = host.file('/tmp/jdk-11_linux-x64_bin.tar.gz')
+    if host.system_info.distribution == "ubuntu":
+        ext = "tar.gz"
+    elif host.system_info.distribution == "centos":
+        ext = "rpm"
+    f = host.file("/tmp/jdk-11_linux-x64_bin." + ext)
     assert f.exists
 
 
